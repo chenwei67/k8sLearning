6ea68f141221646e01c5eaa678c5d281b7d8309b
chenwei <chenwei67@foxmail.com>
2021-12-12 12:26:24 +0800

    deployment

--- main.go
+++ main.go
@@ -6,18 +6,14 @@ import (
 	"net/http"
 	"os"
 )
-var cnt int
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Received from %s\n", r.RemoteAddr)
 	hostname, err := os.Hostname()
 	if err != nil {
 		fmt.Printf("err: %s", err)
 	}
-	if cnt == 5{
-		_, _ = fmt.Fprintf(w, "error occur %s", hostname)
-	}
-	cnt++
-	_, _ = fmt.Fprintf(w, "You've hit %s", hostname)
+	_, _ = fmt.Fprintf(w, "You've hit %s\n", hostname)
 }
 
 func main() {
